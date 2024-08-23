a9e021b3f10dfaeb2e3dce89ad194f4fc68265fb
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-08-23 16:38:26 +0700

    create query class for raw materials

--- app/Http/Controllers/API/RawMaterialAPIController.php
+++ app/Http/Controllers/API/RawMaterialAPIController.php
@@ -3,9 +3,48 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\RawMaterial;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class RawMaterialAPIController extends Controller
 {
-    //
+
+    private function allBuilder() : QueryBuilder {
+        return QueryBuilder::for(RawMaterial::class)
+           -> allowedIncludes(['suppliers', 'products'])
+           -> allowedFilters([
+              AllowedFilter::exact('id'),
+              AllowedFilter::exact('quantity'),
+              AllowedFilter::exact('unit_price'),
+              AllowedFilter::exact('total_value'),
+              AllowedFilter::exact('minimum_stock_level'),
+              AllowedFilter::exact('unit'),
+              AllowedFilter::exact('package_size'),
+           ])
+           -> allowedSorts('created_at' , 'quantity' , 'package_size' , 'total_value' , 'minimun_stock_level')
+           -> defaultSort('-created_at');
+
+    }
+
+    
+
+    public function index (){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function update (Request $request , $id){
+
+    }
+
+    public function destroy($id){
+
+    }
+
+
 }
