1b0f940a0304a7348605b04139707016b75c64fd
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-09-24 21:08:24 +0700

    add more schema

--- database/migrations/2024_07_28_111413_create_suppliers_table.php
+++ database/migrations/2024_07_28_111413_create_suppliers_table.php
@@ -13,24 +13,32 @@ return new class extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
-            $table -> string('image' , 255) -> nullable();
-            $table -> string('name' , 255);
-            $table -> string('phone_number', 50);
-            $table -> string('location' , 255);
-            $table -> string('longitude' , 100) -> nullable();
-            $table -> string('latitude' , 100) -> nullable();
+            $table->id();
+            $table->string('image', 255)->nullable();
+            $table->string('name', 255);
+            $table->string('supplier_code', 100)->unique(); // New field
+            $table->string('phone_number', 50);
+            $table->string('location', 255);
+            $table->string('longitude', 100)->nullable();
+            $table->string('latitude', 100)->nullable();
             $table->string('address', 255)->nullable();
-            $table->string('city', 100)->nullable();
             $table->string('email', 255)->unique();
             $table->string('contact_person', 255);
+            $table->string('website', 255)->nullable(); // New field
+            $table->string('social_media', 255)->nullable(); // New field
+            $table->string('supplier_category', 255)->nullable(); // New field
+            $table->string('supplier_status', 100)->nullable(); // New field
+            $table->string('contract_length', 100)->nullable(); // New field
+            $table->string('discount_term', 100)->nullable(); // New field
+            $table->string('payment_term', 100)->nullable(); // New field
             $table->string('business_registration_number', 100)->nullable();
-            $table->string('vat_number', 100)->nullable(); 
+            $table->string('vat_number', 100)->nullable();
             $table->string('bank_account_number', 50)->nullable();
             $table->string('bank_account_name', 50)->nullable();
             $table->string('bank_name', 255)->nullable();
-            $table -> text('note') -> nullable();
+            $table->text('note')->nullable();
             $table->timestamps();
-            $table -> softDeletes();
+            $table->softDeletes();
         });
     }
 
