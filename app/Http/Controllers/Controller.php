79606853fcc4888b095788518fdf8ba28a363bab
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2025-01-03 01:28:28 +0700

    api documentation with swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *    title="GIZ Inventory System API",
+ *    description="GIZ Inventory System API Documentation",
+ *    version="1.0.0",
+ * )
+ */
 
 class Controller extends BaseController
 {
